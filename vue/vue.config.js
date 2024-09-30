352b0f18c6ff35a0488b0439953c5e2e6fa1fc25
Ryan Alex Martin <ryan.alex.martin@gmail.com>
2024-09-30 17:00:01 +0800

    fix env for frontend for both dev and build

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const path = require('path');
+
+// Determine which .env file to use
+const envPath = path.resolve(__dirname, '../.env');
+
+// Load environment variables
+const env = dotenv.config({ path: envPath }).parsed || {};
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/',
 })
+
