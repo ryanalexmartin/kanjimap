0ce1c0f8fa233be5559c815fe8cb35d1897975f0
Ryan Alex Martin <ryan.alex.martin@gmail.com>
2024-09-30 16:25:04 +0800

    fix docker entrypoint

--- sql/docker-entrypoint.sh
+++ sql/docker-entrypoint.sh
@@ -5,17 +5,19 @@ set -e
 docker-entrypoint.sh mysqld &
 
 # Wait for MySQL to be ready
-until mysqladmin ping -h"localhost" --silent; do
-    echo 'Waiting for MySQL to be ready...'
-    sleep 2
-done
-
-echo "MySQL is ready. Running update_characters_table.py..."
+# until mysqladmin ping -h"localhost" --silent; do
+#     echo 'Waiting for MySQL to be ready...'
+#     sleep 2
+# done
+#
+# echo "MySQL is ready. Running update_characters_table.py..."
 
 # Run the Python script
 python3 /docker-entrypoint-initdb.d/update_characters_table.py
-
 echo "update_characters_table.py completed."
+python3 /docker-entrypoint-initdb.d/import_frequency_data.py
+echo "import_frequency_data.py completed."
+
 
 # Keep the container running with MySQL in the foreground
 wait $!
